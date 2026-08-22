af62c26cb48026833616102e2d8837d705fb1ac4
3dcd <151972449+3dcd@users.noreply.github.com>
2026-08-22 09:42:32 -0300

    Add files via upload

--- blanca_asistente_nutricion/sw.js
+++ blanca_asistente_nutricion/sw.js
@@ -1,10 +1,34 @@
-const CACHE='blanca-asistente-colores-v2';
+const CACHE='blanca-almuerzo-previews-v1';
 const ASSETS=[
   './',
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './images/almuerzo/almuerzo_01_thumb.jpg',
+  './images/almuerzo/almuerzo_01.jpg',
+  './images/almuerzo/almuerzo_02_thumb.jpg',
+  './images/almuerzo/almuerzo_02.jpg',
+  './images/almuerzo/almuerzo_03_thumb.jpg',
+  './images/almuerzo/almuerzo_03.jpg',
+  './images/almuerzo/almuerzo_04_thumb.jpg',
+  './images/almuerzo/almuerzo_04.jpg',
+  './images/almuerzo/almuerzo_05_thumb.jpg',
+  './images/almuerzo/almuerzo_05.jpg',
+  './images/almuerzo/almuerzo_06_thumb.jpg',
+  './images/almuerzo/almuerzo_06.jpg',
+  './images/almuerzo/almuerzo_07_thumb.jpg',
+  './images/almuerzo/almuerzo_07.jpg',
+  './images/almuerzo/almuerzo_08_thumb.jpg',
+  './images/almuerzo/almuerzo_08.jpg',
+  './images/almuerzo/almuerzo_09_thumb.jpg',
+  './images/almuerzo/almuerzo_09.jpg',
+  './images/almuerzo/almuerzo_10_thumb.jpg',
+  './images/almuerzo/almuerzo_10.jpg',
+  './images/almuerzo/almuerzo_11_thumb.jpg',
+  './images/almuerzo/almuerzo_11.jpg',
+  './images/almuerzo/almuerzo_12_thumb.jpg',
+  './images/almuerzo/almuerzo_12.jpg'
 ];
 
 self.addEventListener('install', event => {
